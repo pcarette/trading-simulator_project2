f8b4934b00fd8dd96702b20855da6da06e3fd57e
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-06-13 17:49:37 +0200

    route asset

--- routes/asset.routes.js
+++ routes/asset.routes.js
@@ -1,14 +1,35 @@
+const Asset = require("../models/Asset.model");
 const router = require("express").Router();
-// require Asset
+
+// Create one specific asset
+router.post("/", async (req, res, next) => {
+  try {
+    req.body
+    const newAsset = Asset.create(req.body);
+    res.status(200).json(newAsset);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// Read all assets
+router.get("/", async (req, res, next) => {
+  try {
+    const allAssets = await Asset.find();
+    res.status(200).json(allAssets);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 // Read one specific asset
 router.get("/:id", async (req, res, next) => {
   try {
-    const id = req.params.id
+    const id = req.params.id;
     const asset = await Asset.findById(id);
     res.status(200).json(asset);
-
-    // To do : update user cash
   } catch (error) {
     console.log(error);
     next(error);
@@ -18,9 +39,9 @@ router.get("/:id", async (req, res, next) => {
 // Update one asset (value)
 router.patch("/:id", async (req, res, next) => {
   try {
-    const id = req.params.id
-    const update = req.body
-    const asset = await Asset.findByIdAndUpdate(id, update, {new: true});
+    const id = req.params.id;
+    const update = req.body;
+    const asset = await Asset.findByIdAndUpdate(id, update, { new: true });
     res.status(200).json(asset);
 
     // update user cash
@@ -33,7 +54,7 @@ router.patch("/:id", async (req, res, next) => {
 // Delete one asset (value)
 router.delete("/:id", async (req, res, next) => {
   try {
-    const id = req.params.id
+    const id = req.params.id;
     const asset = await Asset.findByIdAndDelete(id);
     res.status(200).json(asset);
 
