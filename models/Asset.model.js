5981d8c8491f11074e4ed3d53781b41827635a4b
Pierre Carette <pierre.carette78@gmail.com>
2022-06-15 11:25:16 +0200

    The stock API is linked but the seed isn't ready

--- models/Asset.model.js
+++ models/Asset.model.js
@@ -1,34 +1,49 @@
 const { Schema, model, SchemaTypes } = require("mongoose");
-const axios = require('axios');
+const axios = require("axios");
+require("dotenv/config");
 
 
 const assetSchema = new Schema(
-    {
-        category: { type: String, enum: ["Crypto", "Stock", "Raw Material"] },
-        
-        name: String, //Example : Bitcoin
-        symbol: String, //Example : BTC
-        value: Number,
-        image: String,
-    },
-    {
-        // this second object adds extra properties: `createdAt` and `updatedAt`
-        timestamps: true,
-    }
-    );
-    
-    assetSchema.methods.calculateAssetValue = async function calculateAssetValue() {
-        //To fetch with the API
-          let promiseAssetValue = await axios(
-            `https://api.binance.com/api/v3/avgPrice?symbol=${this.symbol}USDT`
-          );
-          console.log(promiseAssetValue.data.price);
-          return promiseAssetValue.data.price
+  {
+    category: { type: String, enum: ["Crypto", "Stock", "Raw Material"] },
+
+    name: String, //Example : Bitcoin
+    symbol: String, //Example : BTC
+    image: String,
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+assetSchema.methods.calculateAssetValue = async function calculateAssetValue() {
+  //To fetch with the API
+  if (this.category == "Crypto") {
+      try{
+
+          const promiseAssetValue = await axios(
+              `https://api.binance.com/api/v3/avgPrice?symbol=${this.symbol}USDT`
+              );
+              console.log(promiseAssetValue.data.price);
+              return promiseAssetValue.data.price;
+        } catch(err) {
+            console.log(err)
         }
-        ;
-       
-        
-        const Asset = model("Asset", assetSchema);
-        
-        module.exports = Asset;
-        
+  } else if (this.category == "Stock") {
+    try{
+        const TOKEN = process.env.IEXAPIKEY;
+        const promiseAssetValue = await axios(
+            `https://cloud.iexapis.com/stable/tops?token=${TOKEN}&symbols=${this.symbol}`
+            );
+           console.log(promiseAssetValue.data[0].lastSalePrice);
+           return promiseAssetValue.data[0].lastSalePrice;
+        } catch (err) {
+            console.log(err);
+        }
+  }
+};
+
+const Asset = model("Asset", assetSchema);
+
+module.exports = Asset;
