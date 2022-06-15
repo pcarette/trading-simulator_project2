154899c003d87cc2f06c5cdf7d85c9c0b15e5d75
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-06-15 12:41:28 +0200

    replace transaction route by Buy route and Sell route

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -12,73 +12,95 @@ const {
 } = require("../helpers");
 
 //
-// Create one transaction (update cash, create/update holding)
+// Buy route
 //
-router.post("/", isAuthenticated, async (req, res, next) => {
+router.post("/buy", isAuthenticated, async (req, res, next) => {
   try {
-    const newTransaction = req.body;
+    const { asset, amount } = req.body;
+    console.log({ asset });
     const userId = req.user._id;
-    const assetId = newTransaction.asset;
     const { cash } = await User.findById(userId);
-    const holding = await Holding.findOne({ user: userId, asset: assetId });
-    const transactionType = newTransaction.transactionType;
-    const myAsset = await Asset.findById(assetId);
+    const holding = await Holding.findOne({ user: userId, asset });
+    const myAsset = await Asset.findById(asset);
+    console.log({ myAsset });
     const myAssetValue = await myAsset.calculateAssetValue();
+    console.log({ myAssetValue });
 
     // Check if enough cash
-    if (transactionType === "BUY") {
-      if (cash < myAssetValue * newTransaction.amount) {
-        res.status(401).json({ message: `You don't have enough cash` });
-        return;
-      }
+    if (cash < myAssetValue * amount) {
+      res.status(401).json({ message: `You don't have enough cash` });
+      return;
     }
-
-    // Check if enough holding
-    if (transactionType === "SELL") {
-      if (!holding) {
-        res.status(401).json({ message: `You don't own any holdings` });
-        return;
-      }
-      if (holding.amount < newTransaction.amount) {
-        res.status(401).json({ message: `You don't own enough holdings` });
-        return;
-      }
-    }
-
     // Create Transaction
     const createdTransaction = await createTransaction(
-      newTransaction,
+      (transactionType = "BUY"),
+      amount,
       userId,
+      asset,
       myAssetValue
     );
 
     // Update user cash
-    await updateCash(createdTransaction.amount, myAssetValue, cash, userId);
+    await updateCash(userId, cash, createdTransaction.amount, myAssetValue);
 
-    //
-    // TO do : Create OR Update holding
-    //
-    if (transactionType === "BUY") {
-      if (!holding) {
-        const createdHolding = await createHolding(
-          userId,
-          assetId,
-          createdTransaction
-        );
-        res.status(200).json({ createdTransaction, createdHolding });
-        return;
-      } else {
-        const updatedHolding = await updateHolding(holding, createdTransaction);
-        res.status(200).json({ createdTransaction, updatedHolding });
-        return;
-      }
-    }
-
-    if (transactionType === "SELL") {
-      const updatedHolding = updateHolding(holding, createdTransaction);
+    // Create OR Update holding
+    if (!holding) {
+      const createdHolding = await createHolding(
+        userId,
+        asset,
+        createdTransaction
+      );
+      res.status(200).json({ createdTransaction, createdHolding });
+      return;
+    } else {
+      const updatedHolding = await updateHolding(holding, createdTransaction);
       res.status(200).json({ createdTransaction, updatedHolding });
       return;
     }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//
+// Sell route
+//
+router.post("/sell", isAuthenticated, async (req, res, next) => {
+  try {
+    const { asset, amount } = req.body;
+    const userId = req.user._id;
+    const { cash } = await User.findById(userId);
+    const holding = await Holding.findOne({ user: userId, asset: asset });
+    const myAsset = await Asset.findById(asset);
+    const myAssetValue = await myAsset.calculateAssetValue();
+
+    // Check if enough holding
+    if (!holding) {
+      res.status(401).json({ message: `You don't own any holdings` });
+      return;
+    }
+    if (holding.amount < Math.abs(amount)) {
+      res.status(401).json({ message: `You don't own enough holdings` });
+      return;
+    }
+
+    // Create Transaction
+    const createdTransaction = await createTransaction(
+      (transactionType = "SELL"),
+      amount,
+      userId,
+      asset,
+      myAssetValue
+    );
+
+    // Update user cash
+    await updateCash(userId, cash, createdTransaction.amount, myAssetValue);
+
+    // Update holding
+    const updatedHolding = await updateHolding(holding, createdTransaction);
+    res.status(200).json({ createdTransaction, updatedHolding });
+    return;
   } catch (error) {
     console.log(error);
     next(error);
