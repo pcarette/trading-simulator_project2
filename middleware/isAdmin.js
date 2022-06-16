86da06ba3e72548a9e070d3343767e2221a598b0
pcarette <54285721+pcarette@users.noreply.github.com>
2022-06-16 15:49:59 +0200

    Update isAdmin.js
    I delete the console.log

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -2,7 +2,7 @@ const isAuthenticated = require("./isAuthenticated");
 
 const isAdmin = async (req, res, next) => {
   const { role } = req.user;
-  console.log(req.user);
+
   if (role !== "admin") {
     res.status(403).json({
       message: "You are not an admin",
